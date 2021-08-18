7b6876ea2e87ab295b75a0e2d083a7550b177b63
Adam R. Gerhartz <adamrussellgerhartz@gmail.com>
2021-08-17 23:56:12 -0600

    Add isMatchingBrackets function and unit tests

---
+++ isMatchingCurlyBrackets.js
@@ -0,0 +1,46 @@
+function isMatchingBrackets(str) {
+
+	// length 0
+	if (str.length == 0) {
+		return true;
+	} 
+
+	// length 1
+	if (str.length == 1) { 
+		if (isBracket(str)) {
+			return false;
+		}
+		return true;
+	}
+
+	// get rid of anything that is not a bracket
+	const regex = /[^\{\}]+/g;
+	str = str.replace(regex, "");
+	
+	// can't have an open bracket before a closing bracket
+	let pFirst = str[0];
+	if (pFirst == "}") {
+		return false;
+	}
+
+	// find and exclude bracket-pairs — "{}"
+	let i = 0;
+	while (pFirst == "{") {
+		// move pointer until bracket-pair is found
+		pFirst = str[++i];
+	}
+	// 1) exclude bracket-pair,
+	// 2) concat remaining right into remaning left, 
+	// 3) and run recursion
+	const remainingLeft  = str.slice(0, i - 1);  // left of opening bracket — "{""
+	const remainingRight = str.slice(i + 1);     // right of opening bracket — "}"
+	return isMatchingBrackets(remainingLeft + remainingRight);
+
+}
+
+// simple helper function
+function isBracket(str) {
+	return str == "{" || str == "}"; 
+}
+
+module.exports = { isMatchingBrackets: isMatchingBrackets }
---
+++ test.js
@@ -0,0 +1,25 @@
+const programmingTest = require("./isMatchingCurlyBrackets.js");
+var assert = require('assert');
+
+
+describe('Unit Tests — isMatchingBrackets(str) function', () => {
+       
+       runUnitTest("{}", true);
+       runUnitTest("}{", false);
+       runUnitTest("{{}", false);
+       runUnitTest("", true);
+       runUnitTest("{abc...xyz}", true);
+       runUnitTest("{{}}{}{{{}{{}}{}}}", true);
+       runUnitTest("{{{{{{{{", false);
+       runUnitTest("}}}}}}}", false);
+       runUnitTest("{asdc{asdcsd}}{asdcasdcadsc}{{{asdcasdc}{{}}.!@#@#@!!@#{}}}", true);
+       runUnitTest("this is my unit test", true);
+
+});
+
+
+function runUnitTest(argument, expected) {
+       it(`should return ${expected} when passing — "${argument}" — as an argument`, () => {
+              assert.equal(programmingTest.isMatchingBrackets(argument), expected);       
+       });
+}
